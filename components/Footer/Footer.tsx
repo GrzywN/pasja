e7d86537ed40692241536a77e55b19f46704ee63
Karol Binkowski <starystachu9@gmail.com>
2022-11-13 16:58:38 +0100

    change favicon

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="relative w-full bg-black">
       <nav className="flex flex-col-reverse items-center px-8 py-32 mx-auto max-w-7xl gap-12 md:justify-between md:gap-4 md:py-12 md:flex-row">
-        <p className="absolute w-full text-sm font-medium text-center bottom-2 text-neutral-900 dark:text-neutral-400 transition-colors md:text-base md:text-left md:static md:w-auto">
+        <p className="absolute w-full text-sm font-medium text-center bottom-2 text-neutral-400 md:text-base md:text-left md:static md:w-auto">
           Projekt i wykonanie: {" "}
           <a
             className="underline text-neutral-200 hover:text-neutral-200 focus-visible:text-neutral-200"
