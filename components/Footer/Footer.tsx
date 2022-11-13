0dcb9b88df9cae3a6a5834ce86b3e07a62dc8a20
Karol Binkowski <starystachu9@gmail.com>
2022-11-13 16:43:28 +0100

    add Footer and favicon

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {IconArrowUp} from "@tabler/icons"
+
+function Footer() {
+  return (
+    <footer className="relative w-full bg-black">
+      <nav className="flex flex-col-reverse items-center px-8 py-32 mx-auto max-w-7xl gap-12 md:justify-between md:gap-4 md:py-12 md:flex-row">
+        <p className="absolute w-full text-sm font-medium text-center bottom-2 text-neutral-900 dark:text-neutral-400 transition-colors md:text-base md:text-left md:static md:w-auto">
+          Projekt i wykonanie: {" "}
+          <a
+            className="underline text-neutral-200 hover:text-neutral-200 focus-visible:text-neutral-200"
+            href="https://karolbinkowski.me/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Karol Binkowski
+          </a>
+        </p>
+        <button className="inline-flex gap-2 text-white font-medium" type="button" onClick={() => {
+          if (typeof window !== undefined) window.scrollTo({ top: 0, behavior: "smooth"});
+        }}>
+          Powróć do góry
+          <IconArrowUp />
+        </button>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,25 @@
 import React from "react";
+import Head from "next/head";
 import Hero from "../components/Hero";
 import About from "../components/About"
 import Location from "../components/Location";
 import Appointment from "../components/Appointment";
+import Footer from "../components/Footer";
 
 function HomePage() {
   return (
     <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <Hero />
       <About />
       <Location />
       <Appointment />
+      <Footer />
     </>
   );
 }
