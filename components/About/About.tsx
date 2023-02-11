264c286db11162df5f74938f927cd897ba12f355
Karol Binkowski <starystachu9@gmail.com>
2023-02-11 09:48:35 +0100

    refactor: make About section's content be passed by props

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,42 +1,52 @@
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import Cta from '../Cta';
 
-import imageAbout from '../../public/images/about.jpg';
+interface AboutProps {
+  sectionTitle: string;
+  sectionId: string;
+  imageSrc: StaticImageData;
+  imageAlt: string;
+  heading: string | React.ReactNode;
+  paragraphs: string[] | React.ReactNode[];
+  ctaButtonText: string;
+  ctaButtonDestination: string;
+}
 
-function About() {
+function About(props: AboutProps) {
+  const {
+    sectionTitle,
+    sectionId,
+    imageSrc,
+    imageAlt,
+    heading,
+    paragraphs,
+    ctaButtonText,
+    ctaButtonDestination,
+  } = props;
+
   return (
     <section
       className="min-h-screen w-full bg-white pt-16 sm:flex sm:items-center sm:pt-0"
-      aria-label="O nas"
-      id="about"
+      aria-label={sectionTitle}
+      id={sectionId}
     >
       <div className="mx-auto flex max-w-5xl flex-col-reverse items-center gap-16 px-8 py-16 sm:grid sm:grid-cols-2 lg:max-w-6xl 2xl:max-w-7xl">
         <Image
           className="w-full shadow-2xl lg:scale-110 xl:h-auto xl:w-3/4 xl:scale-[1.175] 2xl:scale-125"
-          src={imageAbout}
-          alt=""
+          src={imageSrc}
+          alt={imageAlt}
           sizes="(max-width: 768px) 95vw, (min-width: 640px) 40vw, (min-width: 1024px) 50vw"
         />
         <div className="relative mx-auto grid w-full gap-6 border border-black p-8 lg:gap-12">
-          <h2 className="text-4xl font-extralight tracking-tight text-black transition sm:text-6xl lg:text-7xl xl:text-8xl">
-            O nas
+          <h2 className="text-4xl font-extralight tracking-tight text-black sm:text-6xl lg:text-7xl xl:text-8xl">
+            {heading}
           </h2>
           <p className="grid max-w-prose gap-4 text-neutral-800 xl:gap-5 xl:text-left xl:text-xl">
-            <span>
-              Jako zgrany zespół świadczymy{' '}
-              <strong>kompleksowe usługi fryzjerskie oraz kosmetyczne</strong>.
-            </span>
-            <span>
-              Dzięki zaangażowaniu i kursom stale powiększamy{' '}
-              <strong>zakres wiedzy</strong>, aby zapewnić{' '}
-              <strong>jak najlepsze wrażenia</strong> dla naszych klientów.
-            </span>
-            <span>
-              Korzystamy z gamy <strong>przetestowanych</strong> przez nas
-              produktów <strong>najwyższej jakości</strong>.
-            </span>
+            {paragraphs.map((paragraph: string | React.ReactNode, index) => (
+              <span key={index}>{paragraph}</span>
+            ))}
           </p>
-          <Cta text="Zobacz gdzie jesteśmy" destination="#location" />
+          <Cta text={ctaButtonText} destination={ctaButtonDestination} />
         </div>
       </div>
     </section>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import Hero from '../components/Hero';
 import Location from '../components/Location';
 import Meta from '../components/Meta';
 
+import imageAbout from '../public/images/about.jpg';
+
 function HomePage() {
   return (
     <>
@@ -31,7 +33,30 @@ function HomePage() {
           ctaButtonText="Przeczytaj coś o nas"
           ctaButtonDestination="#about"
         />
-        <About />
+        <About
+          sectionTitle="O nas"
+          sectionId="about"
+          imageSrc={imageAbout}
+          imageAlt="Fryzjer wykonujący usługę kręcenia loków dla klienta."
+          heading="O nas"
+          paragraphs={[
+            <>
+              Jako zgrany zespół świadczymy{' '}
+              <strong>kompleksowe usługi fryzjerskie oraz kosmetyczne</strong>.
+            </>,
+            <>
+              Dzięki zaangażowaniu i kursom stale powiększamy{' '}
+              <strong>zakres wiedzy</strong>, aby zapewnić{' '}
+              <strong>jak najlepsze wrażenia</strong> dla naszych klientów.
+            </>,
+            <>
+              Korzystamy z gamy <strong>przetestowanych</strong> przez nas
+              produktów <strong>najwyższej jakości</strong>.
+            </>,
+          ]}
+          ctaButtonText="Zobacz gdzie jesteśmy"
+          ctaButtonDestination="#location"
+        />
         <Location />
         <Appointment />
       </main>
