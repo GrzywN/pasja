397c33aa0d45687b1305a411a10bbbc57cbeea6c
Karol Binkowski <starystachu9@gmail.com>
2023-02-11 09:09:40 +0100

    refactor: make Hero section's content be passed by props

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,20 +1,39 @@
-import React from "react";
-import Cta from "../Cta";
+import React from 'react';
+import Cta from '../Cta';
 
-function Hero() {
+interface HeroProps {
+  sectionTitle: string;
+  sectionId: string;
+  heading: string | React.ReactNode;
+  paragraph: string | React.ReactNode;
+  ctaButtonText: string;
+  ctaButtonDestination: string;
+}
+
+function Hero(props: HeroProps) {
+  const {
+    sectionTitle,
+    sectionId,
+    heading,
+    paragraph,
+    ctaButtonText,
+    ctaButtonDestination,
+  } = props;
 
   return (
-    <section className="w-full h-screen pt-16 sm:pt-0 sm:flex sm:items-center" aria-label="Sekcja hero" id="hero">
+    <section
+      className="w-full h-screen pt-16 sm:pt-0 sm:flex sm:items-center"
+      aria-label={sectionTitle}
+      id={sectionId}
+    >
       <div className="relative px-8 mx-auto w-full max-w-6xl grid gap-6 lg:gap-12 2xl:max-w-7xl">
         <h1 className="text-4xl font-extralight tracking-tight transition text-white sm:text-6xl lg:text-7xl 2xl:text-8xl">
-          Studio Fryzur{" "}
-          <strong className="font-bold">Pasja</strong>
+          {heading}
         </h1>
         <p className="max-w-prose text-neutral-200 sm:text-xl xl:text-left 2xl:text-2xl">
-          Stanowimy zgraną ekipę i zapewniamy <strong>kompleksowe usługi fryzjerskie</strong> na miarę potrzeb klienta.
-          Stawiamy na <strong>najnowsze trendy i stylizacje</strong>, a oprócz usług stylizacyjnych włosów, oferujemy również <strong>zabiegi regenerujące</strong>.
+          {paragraph}
         </p>
-        <Cta text="Przeczytaj coś o nas" destination="#about" />
+        <Cta text={ctaButtonText} destination={ctaButtonDestination} />
       </div>
     </section>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,37 @@
-import React from "react";
-import Meta from "../components/Meta";
-import Hero from "../components/Hero";
-import About from "../components/About"
-import Location from "../components/Location";
-import Appointment from "../components/Appointment";
-import Footer from "../components/Footer";
+import React from 'react';
+import Meta from '../components/Meta';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Location from '../components/Location';
+import Appointment from '../components/Appointment';
+import Footer from '../components/Footer';
 
 function HomePage() {
   return (
     <>
       <Meta />
-
       <main>
-        <Hero />
+        <Hero
+          sectionTitle="Wprowadzenie"
+          sectionId="hero"
+          heading={
+            <>
+              Studio Fryzur <strong className="font-bold">Pasja</strong>
+            </>
+          }
+          paragraph={
+            <>
+              Stanowimy zgraną ekipę i zapewniamy{' '}
+              <strong>kompleksowe usługi fryzjerskie</strong> na miarę potrzeb
+              klienta. Stawiamy na{' '}
+              <strong>najnowsze trendy i stylizacje</strong>, a oprócz usług
+              stylizacyjnych włosów, oferujemy również{' '}
+              <strong>zabiegi regenerujące</strong>.
+            </>
+          }
+          ctaButtonText="Przeczytaj coś o nas"
+          ctaButtonDestination="#about"
+        />
         <About />
         <Location />
         <Appointment />
